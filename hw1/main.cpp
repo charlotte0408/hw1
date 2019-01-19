b53b664f7d590a7ca38432d5744a29d35aaae7a1
Charlotte Huang <charlotte0408@g.ucla.edu>
2019-01-19 09:27:22 -0800

    Declare variables for draw cards function

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -8,6 +8,14 @@ using namespace std;
 
 int draw_card (string &card, int current_total);
 
+int draw_card (string &card, int current_total)
+{
+    //declare variables
+    string ranks, suits;
+    int rank = rand()%(13) + 1;
+    int suit = rand()%(4) + 1;
+}
+
 int main() {
     
     return 0;
