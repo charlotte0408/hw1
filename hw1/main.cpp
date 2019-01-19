2a26714aa7d41afe16589eba43cb019baab7c2cd
Charlotte Huang <charlotte0408@g.ucla.edu>
2019-01-19 09:31:21 -0800

    Complete the rest of calculating totals

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -60,6 +60,25 @@ int draw_card (string &card, int current_total)
         else
             return 11;
     }
+    else if (rank == 2)
+        return 2;
+    else if (rank == 3)
+        return 3;
+    else if (rank == 4)
+        return 4;
+    else if (rank == 5)
+        return 5;
+    else if (rank == 6)
+        return 6;
+    else if (rank == 7)
+        return 7;
+    else if (rank == 8)
+        return 8;
+    else if (rank == 9)
+        return 9;
+    else
+        return 10;
+    
 }
 
 int main() {
