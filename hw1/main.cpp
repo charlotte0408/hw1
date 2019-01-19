f26e8d45c21d0fe36375c5d46f155177874f0949
Charlotte Huang <charlotte0408@g.ucla.edu>
2019-01-19 09:28:05 -0800

    Implement the rank part of the draw cards function

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -14,6 +14,35 @@ int draw_card (string &card, int current_total)
     string ranks, suits;
     int rank = rand()%(13) + 1;
     int suit = rand()%(4) + 1;
+    
+    //assign values to variables
+    if (rank == 1)
+        ranks = "Ace";
+    else if (rank == 2)
+        ranks = "Two";
+    else if (rank == 3)
+        ranks = "Three";
+    else if (rank == 4)
+        ranks = "Four";
+    else if (rank == 5)
+        ranks = "Five";
+    else if (rank == 6)
+        ranks = "Six";
+    else if (rank == 7)
+        ranks = "Seven";
+    else if (rank == 8)
+        ranks = "Eight";
+    else if (rank == 9)
+        ranks = "Nine";
+    else if (rank == 10)
+        ranks = "Ten";
+    else if (rank == 11)
+        ranks = "Jack";
+    else if (rank == 12)
+        ranks = "Queen";
+    else
+        ranks = "King";
+    
 }
 
 int main() {
